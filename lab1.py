27739d8f7def1d03360d213b49225a19d60967cc
Linad <k_linad@mail.ru>
2022-10-26 15:32:30 +0300

    initial commit

---
+++ lab1.py
@@ -0,0 +1,19 @@
+'''def sort(list_1):
+    for n in range(len(list_1)-1):
+        for i in range(len(list_1) - n - 1):
+            if list_1[i] > list_1[i + 1]:
+                larg = list_1[i]
+                list_1[i] = list_1[i + 1]
+                list_1[i + 1] = larg
+def main():
+    list_1 = [14, 21, 32, 40, 5, 61, 7, 82, 91, 6, 4, 3, 64, 56, 11, 74, 23, 39, 97, 22]
+    list_2 = []
+    print('Исходный список:',list_1)
+    sort(list_1)
+    list_2=list_1.copy()
+    print('Отсортированный список:',list_2)
+main()'''
+list_1=[1,2,3,4,5,6,7]
+list_2=list_1.reverse
+)
+print(list_2)
